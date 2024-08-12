445aa056d58e154bd5e3679239883d7b1f0df4fb
mkorrea <mathscorrea@gmail.com>
2024-08-12 17:24:57 -0300

    create full html and css of main create room page

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,6 +1,14 @@
+import { ArrowRight } from "lucide-react"
 import amaLogo from "../assets/ama-logo.svg"
+import { useNavigate } from "react-router-dom"
 
 export function CreateRoom(){
+  const navigate = useNavigate()
+  
+  function handleCreateRoom() {
+    navigate('/room/sadadw')
+  }
+   
   return (
     <main className="h-screen flex items-center justify-center px-4">
       <div className="max-w-[450px] flex flex-col gap-6">
@@ -10,18 +18,25 @@ export function CreateRoom(){
           Create a public AMA (Ask Me Anything) room e prioritize the most important question for the community 
         </p>
 
-        <form>
-          <div className="bg-zinc-900 rounded-xl p-2 flex">
-            <input 
-              type="text" 
-              placeholder="Room's name"
-              className="bg-transparent flex flex-1 px-2.5" 
-            />
-            <button type="submit" className="bg-orange-400 text-orange-950 px-3 py-1.5 rounded-lg font-medium"> 
-              Create room 
-              
-            </button>
-          </div>
+        <form 
+          className="flex items-center gap-2 bg-zinc-900 rounded-xl p-2 border border-zinc-800 ring-orange-400 ring-offset-2 ring-offset-zinc-950 focus-within:ring-1"
+          onSubmit={handleCreateRoom}
+        >
+          <input 
+            type="text" 
+            name="theme"
+            autoComplete="off"
+            placeholder="Room's name"
+            className="bg-transparent flex flex-1 text-sm mx-2 outline-none text-zinc-100 placeholder:text-zinc-500" 
+          />
+          <button 
+            type="submit" 
+            className="bg-orange-400 text-orange-950 px-3 py-1.5 gap-1.5 rounded-lg font-medium flex items-center text-sm transition-colors hover:bg-orange-500"
+          > 
+            Create room 
+            <ArrowRight className="size-4" />
+          </button>
+          
         </form>
         
       </div>
