5183d2deeec66389ae61e55054c2bddc9a7d688e
mkorrea <mathscorrea@gmail.com>
2024-08-14 00:00:20 -0300

    install react 19

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -5,7 +5,10 @@ import { useNavigate } from "react-router-dom"
 export function CreateRoom(){
   const navigate = useNavigate()
   
-  function handleCreateRoom() {
+  function handleCreateRoom(data: FormData) {
+    const theme = data.get('theme')?.toString()
+    console.log(theme)
+    
     navigate('/room/sadadw')
   }
    
@@ -19,8 +22,8 @@ export function CreateRoom(){
         </p>
 
         <form 
+          action={handleCreateRoom}
           className="flex items-center gap-2 bg-zinc-900 rounded-xl p-2 border border-zinc-800 ring-orange-400 ring-offset-2 ring-offset-zinc-950 focus-within:ring-1"
-          onSubmit={handleCreateRoom}
         >
           <input 
             type="text" 
